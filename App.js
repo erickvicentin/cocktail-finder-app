aa1b0a8fa68c9ed10ade182fec1f7a7b36b25f07
Erick Emanuel Vicentin <ErickVicentin.Globant@latam.com>
2020-11-20 18:52:14 -0300

    First commit

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from "expo-linear-gradient";
+import React, { useState } from "react";
+import { StyleSheet, Button, Text, View, TextInput } from "react-native";
+
 
 export default function App() {
+
+  const handleText = (str) => {
+    console.log(str);
+  };
+
+  const [string, setString] = useState("Cocktail-Finder!");
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <LinearGradient
+        colors={["violet", "red", "orange"]}
+        style={styles.gradient}
+      >
+        <Text style={{ color: "white", fontSize: 24 }}>{string}</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Type here..."
+          placeholderTextColor="white"
+          onChangeText={(str) => handleText(setString(str))}
+        />
+      </LinearGradient>
     </View>
   );
 }
@@ -14,8 +32,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "transparent",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    fontSize: 20,
+    marginTop: 10,
+    color: "white",
+    textAlign: "center",
+    width: "95%",
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 2,
+    paddingBottom: 5,
+  },
+  gradient: {
+    alignItems: "center",
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
   },
 });
