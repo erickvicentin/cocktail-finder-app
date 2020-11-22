6d1869c333397408021c1897a35065870af858bd
Erick Emanuel Vicentin <ErickVicentin.Globant@latam.com>
2020-11-22 05:36:07 -0300

    Added fetch & Updated UI

--- App.js
+++ App.js
@@ -1,55 +1,174 @@
+import React, { useState, useEffect } from "react";
 import { LinearGradient } from "expo-linear-gradient";
-import React, { useState } from "react";
-import { StyleSheet, Button, Text, View, TextInput } from "react-native";
+import {
+  FlatList,
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableNativeFeedback,
+  Image,
+  ActivityIndicator
+} from "react-native";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+import Header from './src/components/Header'
 
 
 export default function App() {
 
-  const handleText = (str) => {
-    console.log(str);
+  const [drinks, setDrinks] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [submit, setSubmit] = useState("");
+  const [string, setString] = useState("");
+
+  const handleSearch = (str) => {
+    setString(str);
+    if (string.length > 2) {
+      setSubmit(string);
+    }
   };
 
-  const [string, setString] = useState("Cocktail-Finder!");
+  const clear = () => {
+    console.log(`string: ${string}`);
+    console.log(`submit: ${submit}`);
+    setSubmit("");
+    setString("");
+  };
+
+  useEffect(() => {
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${submit}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setDrinks(data.drinks);
+        setLoading(false);
+      });
+  }, []);
+
+  const handleKeyDown = (e) => {
+    if (e.nativeEvent.key == "Enter") {
+      dismissKeyboard();
+    }
+  };
+
+  if (loading) {
+    return (
+      <View style={styles.loading}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
   
   return (
     <View style={styles.container}>
       <LinearGradient
-        colors={["violet", "red", "orange"]}
-        style={styles.gradient}
+        colors={["#ff5f6d", "#ffc371"]}
+        style={styles.linearGradient}
       >
-        <Text style={{ color: "white", fontSize: 24 }}>{string}</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Type here..."
-          placeholderTextColor="white"
-          onChangeText={(str) => handleText(setString(str))}
-        />
+        <Header />
+        <View style={styles.searchBar}>
+          <Ionicons
+            name="ios-search"
+            size={24}
+            color="white"
+            style={{ flex: 0.5 }}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Search your favorite drink..."
+            placeholderTextColor="white"
+            onChangeText={(str) => handleSearch(str)}
+            onKeyPress={(e) => handleKeyDown(e)}
+            value={string}
+          />
+          <TouchableNativeFeedback
+            style={{ flex: 0.5 }}
+            onPress={() => clear()}
+          >
+            <MaterialIcons name="clear" size={24} color="white" />
+          </TouchableNativeFeedback>
+        </View>
+        <View style={styles.listContainer}>
+          <FlatList
+            data={drinks}
+            renderItem={({ item }) => (
+              <View style={styles.card}>
+                <Image
+                  style={{width: 80, height: 80, borderRadius: 30}}
+                  source={{ uri: item.strDrinkThumb }}
+                />
+                <View>
+                  <Text style={{ paddingLeft: 10 }}>{item.strDrink}</Text>
+                  <Text style={{ paddingLeft: 10 }}>
+                    Categoria: {item.strCategory}
+                  </Text>
+                </View>
+              </View>
+            )}
+            keyExtractor={(item) => item.idDrink}
+          />
+        </View>
       </LinearGradient>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  linearGradient: {
+    flex: 1,
+    alignItems: "stretch",
+    justifyContent: "center",
+  },
   container: {
     flex: 1,
-    backgroundColor: "transparent",
-    alignItems: "center",
+    marginTop: 25,
+    alignItems: "stretch",
     justifyContent: "center",
+  },
+  item: {
+    padding: 10,
+    fontSize: 22,
+    height: 50,
+    borderBottomColor: "black",
+    borderBottomWidth: 2,
+    textAlign: "center",
+  },
+  loading: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  card: {
+    backgroundColor: 'white',
+    paddingLeft: 10,
+    paddingVertical: 10,
+    marginVertical: 5,
+    borderColor: "transparent",
+    borderWidth: 2,
+    borderRadius: 10,
+    flexDirection: 'row',
+    alignItems: "center"
   },
   input: {
-    fontSize: 20,
-    marginTop: 10,
-    color: "white",
-    textAlign: "center",
-    width: "95%",
-    borderBottomColor: "#ccc",
-    borderBottomWidth: 2,
+    flex: 8,
+    paddingHorizontal: 5,
+    paddingTop: 10,
     paddingBottom: 5,
+    color: "white",
+    fontSize: 16,
   },
-  gradient: {
-    alignItems: "center",
-    width: "100%",
-    height: "100%",
+  listContainer: {
+    flex: 1,
+    alignItems: "stretch",
     justifyContent: "center",
+    marginHorizontal: 10,
+  },
+  searchBar: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 10,
+    borderBottomColor: "white",
+    borderBottomWidth: 2,
+    marginVertical: 5,
+    paddingBottom: 5,
   },
 });
